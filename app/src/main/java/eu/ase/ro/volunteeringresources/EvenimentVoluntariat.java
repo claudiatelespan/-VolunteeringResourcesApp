cacd0bfa532ffbe68d09e836bdbd01c8c5a1c9d5
Tascu Daniel <tascudaniel22@stud.ase.ro>
2024-11-02 17:33:43 +0200

    Finalizat Dani -Styles realziat dar nefunctional

--- app/src/main/java/eu/ase/ro/volunteeringresources/EvenimentVoluntariat.java
+++ app/src/main/java/eu/ase/ro/volunteeringresources/EvenimentVoluntariat.java
@@ -1,4 +1,80 @@
 package eu.ase.ro.volunteeringresources;
 
+import java.util.Date;
+
 public class EvenimentVoluntariat {
+    private Integer id;
+    private String numeEveniment;
+    private String numeAsociatie;
+    private Date dataEveniment;
+    private String locatie;
+    private String descriere;
+
+    public String getNumeEveniment() {
+        return numeEveniment;
+    }
+
+    public void setNumeEveniment(String numeEveniment) {
+        this.numeEveniment = numeEveniment;
+    }
+
+    public String getNumeAsociatie() {
+        return numeAsociatie;
+    }
+
+    public void setNumeAsociatie(String numeAsociatie) {
+        this.numeAsociatie = numeAsociatie;
+    }
+
+    public Date getDataEveniment() {
+        return dataEveniment;
+    }
+
+    public void setDataEveniment(Date dataEveniment) {
+        this.dataEveniment = dataEveniment;
+    }
+
+    public String getLocatie() {
+        return locatie;
+    }
+
+    public void setLocatie(String locatie) {
+        this.locatie = locatie;
+    }
+
+    public String getDescriere() {
+        return descriere;
+    }
+
+    public void setDescriere(String descriere) {
+        this.descriere = descriere;
+    }
+
+    public EvenimentVoluntariat(String locatie, String descriere, Date dataEveniment, String numeAsociatie, String numeEveniment, Integer id) {
+        this.locatie = locatie;
+        this.descriere = descriere;
+        this.dataEveniment = dataEveniment;
+        this.numeAsociatie = numeAsociatie;
+        this.numeEveniment = numeEveniment;
+        this.id = id;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Eveniment: " + numeEveniment + "\n" +
+                "Organizat de: " + numeAsociatie + "\n" +
+                "Data: " + DateConverter.fromDate(dataEveniment) + "\n" +
+                "Locatie: " + locatie + "\n" +
+                "Descriere: " + descriere;
+    }
 }
--- app/src/main/java/eu/ase/ro/volunteeringresources/MainActivity.java
+++ app/src/main/java/eu/ase/ro/volunteeringresources/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -20,20 +22,86 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 import eu.ase.ro.volunteeringresources.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private ListView listview;
+    private List<EvenimentVoluntariat> evenimente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listview = findViewById(R.id.tascu_daniel_lv_main_evenimente);
+
+        initializareEvenimente();
+
+        ArrayAdapter<EvenimentVoluntariat> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,evenimente);
+
+        listview.setAdapter(adapter);
+
         configNavigation();
-        navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.tascu_daniel_nav_view);
         navigationView.setNavigationItemSelectedListener(getItemSelectedEvent());
+    }
+
+    private void initializareEvenimente() {
+        evenimente = new ArrayList<>();
+
+        evenimente.add(new EvenimentVoluntariat(
+                "Romexpo",
+                "Workshop Dezvoltare Personala interactiv",
+                DateConverter.toDate("12-01-2024"),
+                "VoluntaRO",
+                "TopUp",
+                1
+        ));
+
+        evenimente.add(new EvenimentVoluntariat(
+                "Palatul Parlamentului",
+                "Conferinta despre antreprenoriat social",
+                DateConverter.toDate("15-01-2024"),
+                "Asociatia Tinerilor Antreprenori",
+                "Start Smart",
+                2
+        ));
+
+        evenimente.add(new EvenimentVoluntariat(
+                "Ateneu",
+                "Concert caritabil pentru sustinerea educatiei",
+                DateConverter.toDate("20-01-2024"),
+                "Fundatia pentru Educatie",
+                "EduConcert",
+                3
+        ));
+
+        evenimente.add(new EvenimentVoluntariat(
+                "Universitatea Politehnica",
+                "Hackathon de solutii sustenabile",
+                DateConverter.toDate("28-01-2024"),
+                "GreenTech Romania",
+                "Eco Hack",
+                4
+        ));
+
+        evenimente.add(new EvenimentVoluntariat(
+                "Parcul Herastrau",
+                "Campanie de plantare copaci si educatie ecologica",
+                DateConverter.toDate("05-02-2024"),
+                "Asociatia Verde",
+                "Plantare pentru Viitor",
+                5
+        ));
+
+
+
     }
 
     private NavigationView.OnNavigationItemSelectedListener getItemSelectedEvent() {
@@ -56,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void configNavigation() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.tascu_daniel_main_toolbar);
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
